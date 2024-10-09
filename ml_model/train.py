823cc5fb65595c3db85f0c47c03eded4b3f4e19c
Suryanshnaithani <suryansh.naithani@gmail.com>
2024-10-09 14:52:34 +0530

    initial commit

--- ml_model/train.py
+++ ml_model/train.py
@@ -4,7 +4,7 @@ from sklearn.cluster import KMeans
 import joblib
 
 def train_model():
-    loaded_data = pd.read_csv('D:\mall-segmentation\ml_model\data\mall.csv')
+    loaded_data = pd.read_csv('ml_model/data/mall.csv')
     X = loaded_data.iloc[:, [3, 4]].values
     kmeans = KMeans(n_clusters=5, init='k-means++', max_iter=300, n_init=10, random_state=0)
     y_kmeans = kmeans.fit_predict(X)
