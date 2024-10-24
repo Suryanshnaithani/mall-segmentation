96f8eeec914f7e25bd8e6b76a92ee540a09711f8
Suryanshnaithani <suryansh.naithani@gmail.com>
2024-10-24 06:47:58 +0530

    initial commit

---
+++ build/lib/ml_model/train.py
@@ -0,0 +1,16 @@
+#training model using mall data on kmeans clustering
+import pandas as pd
+from sklearn.cluster import KMeans
+import joblib
+
+def train_model():
+    loaded_data = pd.read_csv('ml_model/data/mall.csv')
+    X = loaded_data.iloc[:, [3, 4]].values
+    kmeans = KMeans(n_clusters=5, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    y_kmeans = kmeans.fit_predict(X)
+    joblib.dump(kmeans, 'ml_model/kmeans_model.pkl')
+    return kmeans
+
+if __name__ == '__main__':
+    train_model()
+    print('Model trained successfully')
